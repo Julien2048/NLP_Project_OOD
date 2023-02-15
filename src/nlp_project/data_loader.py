d96ab4113552f869eced5a6cd3e56db9a85d8073
Julien Mereau <jumereau62223@gmail.com>
2023-02-15 16:06:19 +0100

    code structure

---
+++ src/nlp_project/data_loader.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+
+class IMDB():
+
+    def __init__(self):
+        pass
+
+    def __call__(self):
+        pass
---
+++ src/nlp_project/metrics.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+def AUROC():
+    pass
+
+def AUPR():
+    pass
+
+def FDR():
+    pass
+
+def ERR():
+    pass
